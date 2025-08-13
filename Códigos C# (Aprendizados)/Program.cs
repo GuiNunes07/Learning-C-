d7834313c09806b5c0c5c7ec084c2fa33cb79e34
Guilherme Nunes Soares <25027440@fecap.br>
2025-08-13 18:48:23 -0300

    feat(Program.cs): Adiciona códigos

---
+++ Códigos C# (Aprendizados)/Program.cs
@@ -0,0 +1,75 @@
+﻿//using System;
+//class HelloWorld
+//{
+//    static void Main()
+//    {
+
+        // Programinha de Nome e idade //    
+
+        // Console.WriteLine("Digite seu nome e idade: ");
+
+        // string nome = Console.ReadLine();
+
+        // int idade = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Hello "+nome+" sua idade é: "+idade);
+
+
+        // -----------------------------------------------------------------------------------//
+
+        // Programinha de Idade + Alistamento //
+
+        // Console.WriteLine("Digite sua idade: ");
+        // int idade = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Sua idade é: "+idade);
+
+        // if((idade>=18)&&(idade<=45))
+        // {
+        //  Console.WriteLine("Vamos fazer alistamento!");
+        //  Console.WriteLine("Qual área deseja alistar-se?");
+        //  Console.WriteLine("1-Infantaria, 2-Cavalaria,3-Tropa Especial, 4-Comunicação");
+
+        //  int area = Convert.ToInt32(Console.ReadLine());
+        //  switch(area)
+        //  {
+        //  case 1:
+        //  Console.WriteLine("Soldado alocado!");
+        //  break;
+
+        //  case 2:
+        //  Console.WriteLine("Suporte alocado!");
+        //  break;
+
+        //  case 3:
+        //  Console.WriteLine("Soldado especial alocado!");
+        //  break;
+
+        //  case 4:
+        //  Console.WriteLine("Comunicador alocado!");
+        //  break;
+
+        //  default:
+
+        //  Console.WriteLine("Área não encontrada!");
+        //  break;
+
+        //  }
+        // }
+        //  for(int i=0; i<10; i++)
+        //  {
+        //      Console.WriteLine(i+1);
+        // }
+
+
+        // -----------------------------------------------------------------------------------//
+
+        // Programinha do For e While - Repete //
+
+        // string irmao = "";
+        // while(irmao!="irmão do grapete")
+        // {
+        //     Console.WriteLine("Existiam dois irmãos, Grapete e Repete. Grapete foi embora, quem ficou? ");
+        //     irmao = Console.ReadLine();
+        // }
+
+
+        // -----------------------------------------------------------------------------------//
